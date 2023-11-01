53ae7700e53062ae929678976a7359d5eb117cd0
dheeraj_700 <dheeraj700agrahari@gmail.com>
2023-11-01 16:34:10 +0530

    first commit

--- client.js
+++ client.js
@@ -1,8 +1,51 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-async function main() {
-  const response = await fetch("http://localhost:3000");
-  const data = await response.json();
-  console.log(data);
+// Function to fetch and print directories
+async function fetchAndPrintDirectories() {
+  try {
+    // Fetch the root directory data
+    const rootResponse = await fetch('http://localhost:3000');
+    const rootData = await rootResponse.json();
+
+    if (rootData.items && rootData.items.length > 0) {
+      for (const item of rootData.items) {
+        if (item.isDir) {
+          // Fetch directory data and print it
+          const directoryData = await fetchDirectory(item.name);
+          console.log(JSON.stringify(directoryData, null, 2));
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching root directory:', error);
+  }
 }
-main();
+
+// Function to fetch directory data
+async function fetchDirectory(directoryName) {
+  try {
+    // Fetch data for a specific directory
+    const directoryResponse = await fetch(`http://localhost:3000/${directoryName}`);
+    const directoryData = await directoryResponse.json();
+
+    const result = {
+      name: directoryName,
+      isDir: true,
+      files: [],
+    };
+
+    if (directoryData.files && directoryData.files.length > 0) {
+      for (const file of directoryData.files) {
+        result.files.push(file);
+      }
+    }
+
+    return result;
+  } catch (error) {
+    console.error(`Error fetching files in directory ${directoryName}:`, error);
+    return {};
+  }
+}
+
+// Call the fetchAndPrintDirectories function to start the process
+fetchAndPrintDirectories();
--- server.js
+++ server.js
@@ -1,59 +1,60 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const port = 3000;
 
 const dirStructure = [
   {
-    name: "file1",
+    name: 'file1',
     isDir: false,
   },
   {
-    name: "dir1",
+    name: 'dir1',
     isDir: true,
     files: [
       {
-        name: "file2",
+        name: 'file2',
         isDir: false,
       },
       {
-        name: "file3",
+        name: 'file3',
         isDir: false,
       },
       {
-        name: "file4",
+        name: 'file4',
         isDir: false,
       },
     ],
   },
   {
-    name: "dir2",
+    name: 'dir2',
     isDir: true,
     files: [
       {
-        name: "file5",
+        name: 'file5',
         isDir: false,
       },
       {
-        name: "file6",
+        name: 'file6',
         isDir: false,
       },
       {
-        name: "file7",
+        name: 'file7',
         isDir: false,
       },
     ],
   },
   {
-    name: "file8",
+    name: 'file8',
     isDir: false,
   },
   {
-    name: "file9",
+    name: 'file9',
     isDir: false,
   },
 ];
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const items = dirStructure.map((item) => ({
     name: item.name,
     isDir: item.isDir,
@@ -61,7 +62,7 @@ app.get("/", (req, res) => {
   res.json({ items });
 });
 
-app.get("/:dir", (req, res) => {
+app.get('/:dir', (req, res) => {
   const { dir } = req.params;
   const item = dirStructure.find((item) => item.name === dir);
 
